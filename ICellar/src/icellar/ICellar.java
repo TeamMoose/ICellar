4513b25670934f221d1b7d8add2d3f31c968431c
Alex Mazzeo <apm59@nau.edu>
2012-04-10 23:56:44 -0700

    First take on building an inventory entry. Needs a little more to be finished. This should be robust enough to use in all wine databases.

---
+++ ICellar/src/icellar/ICellar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package icellar;
+
+/**
+ *
+ * @author Alex Mazzeo
+ */
+public class ICellar 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Comment cm = new Comment("Hello World!", "AMazzeo");
+        System.out.println(cm.getDate());
+    }
+}
