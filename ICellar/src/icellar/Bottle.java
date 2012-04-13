f11673d7af4dacfa726c687b4162e7f1a2571268
Ryan Lessley <darkside889@hotmail.com>
2012-04-12 18:41:59 -0700

    Corrected Package.
    Signed-off-by: Ryan Lessley <darkside889@hotmail.com>

--- ICellar/src/icellar/Bottle.java
+++ ICellar/src/icellar/Bottle.java
@@ -1,3 +1,5 @@
+package icellar;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
--- ICellar/src/icellar/Cellar.java
+++ ICellar/src/icellar/Cellar.java
@@ -1,3 +1,5 @@
+package icellar;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
